a71747e0a4ccdde32c709d1429f064f471129f6d
Daniel S. Buckstein <dbuckstein@users.noreply.github.com>
2023-11-25 01:00:08 -0500

    Implemented remainder of GL basic setup, namely creating the context. MOD: renderer source (WinGL), implemented GL context creation, destruction and attach to window; player and plugin project user settings, need to locate GLEW DLL before plugin load, so adding the DLL path to debug environments for now.

--- cdraw/source/cdraw/cdrawPlatform/_Windows/cdrawRenderer_win/cdrawRenderer_win_gl.c
+++ cdraw/source/cdraw/cdrawPlatform/_Windows/cdrawRenderer_win/cdrawRenderer_win_gl.c
@@ -72,7 +72,120 @@ typedef struct cdrawRenderer_win_gl
 	/// OpenGL pixel format flag.
 	/// </summary>
 	int32_t pf;
+
+	/// <summary>
+	/// Setup flag.
+	/// </summary>
+	BOOL flag;
 } cdrawRenderer_win_gl;
+
+
+static void cdrawRendererInternalWindowSetPixelFormat_win_gl(cdrawRenderer_win_gl* const r)
+{
+	// describe built-in pixel format
+	PIXELFORMATDESCRIPTOR pfd[1] = { 0 };
+	pfd->nSize = sizeof(PIXELFORMATDESCRIPTOR);
+	pfd->nVersion = 1;					// version
+	pfd->dwFlags =
+		PFD_DRAW_TO_WINDOW |			// window drawing
+		PFD_SUPPORT_OPENGL |			// using opengl
+		PFD_DOUBLEBUFFER |				// double buffer support
+		PFD_GENERIC_ACCELERATED;		// hardware acceleration
+	pfd->iPixelType = PFD_TYPE_RGBA;	// RGBA colour mode
+	pfd->cColorBits = 32;				// **32 bit colour buffer
+	pfd->cDepthBits = 24;				// **24 bit depth buffer
+	pfd->cStencilBits = 8;				// **8 bit stencil buffer
+	pfd->iLayerType = PFD_MAIN_PLANE;
+
+	// result
+	int32_t pixelFormat = 0;
+	int32_t numFormats = 0;
+
+	// reset
+	r->flag = 0;
+
+	// if platform repeat flag is raised, then the default format 
+	//	has been previously used; select new pixel format using 
+	//	WGL to get access to new features
+	if (r->pf > 0)
+	{
+		// WGL pixel format descriptor
+		const int32_t pixelAttribs[] = {
+			// standard pixel format settings
+			WGL_DRAW_TO_WINDOW_ARB,				GL_TRUE,
+			WGL_SUPPORT_OPENGL_ARB,				GL_TRUE,
+			WGL_DOUBLE_BUFFER_ARB,				GL_TRUE,
+			WGL_ACCELERATION_ARB,				WGL_FULL_ACCELERATION_ARB,
+			WGL_PIXEL_TYPE_ARB,					WGL_TYPE_RGBA_ARB,
+			WGL_COLOR_BITS_ARB,					040,
+			WGL_DEPTH_BITS_ARB,					030,
+			WGL_STENCIL_BITS_ARB,				010,
+
+			// multi-sampling (MSAA) settings
+			WGL_SAMPLE_BUFFERS_ARB,				GL_FALSE,
+			WGL_SAMPLES_ARB,					000,
+
+			// done
+			0
+		};
+
+		// get pixel format function and use it to select pixel format
+		// this is a good way to validate the address of the function
+		PFNWGLCHOOSEPIXELFORMATARBPROC const wglChoosePixelFormat = wglChoosePixelFormatARB;
+		if (wglChoosePixelFormat)
+			wglChoosePixelFormat(r->dc, pixelAttribs, 0, 1, &pixelFormat, &numFormats);
+	}
+
+	// otherwise, use built-in default descriptor
+	else
+	{
+		pixelFormat = ChoosePixelFormat(r->dc, pfd);
+	}
+
+	// after pixel format is selected, assign it to device context
+	if (pixelFormat)
+		r->flag = SetPixelFormat(r->dc, pixelFormat, pfd);
+
+	// store
+	r->pf = pixelFormat;
+}
+
+static LRESULT __stdcall cdrawRendererInternalWindowEvent_win_gl(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	cdrawRenderer_win_gl* r = (cdrawRenderer_win_gl*)GetWindowLongPtrA(hWnd, GWLP_USERDATA);
+	switch (message)
+	{
+	case WM_NCCREATE: {
+		r = (cdrawRenderer_win_gl*)((LPCREATESTRUCT)lParam)->lpCreateParams;
+		r->dc = GetDC(hWnd);
+		SetWindowLongPtrA(hWnd, GWLP_USERDATA, (LONG_PTR)r);
+	}	break;
+	case WM_CREATE: {
+		cdrawRendererInternalWindowSetPixelFormat_win_gl(r);
+	}	break;
+	case WM_DESTROY: {
+		// DO NOTHING
+	}	break;
+	case WM_NCDESTROY: {
+		ReleaseDC(hWnd, r->dc);
+	}	break;
+	}
+	return DefWindowProcA(hWnd, message, wParam, lParam);
+}
+
+static void cdrawRendererInternalWindowClassInit_win_gl(WNDCLASSA* const wndClass, cstrk_t const name)
+{
+	wndClass->style = 0;
+	wndClass->lpfnWndProc = cdrawRendererInternalWindowEvent_win_gl;
+	wndClass->cbClsExtra = 0;
+	wndClass->cbWndExtra = (int)sizeof(cdrawRenderer_win_gl*);
+	wndClass->hInstance = GetModuleHandle(NULL);
+	wndClass->hIcon = NULL;
+	wndClass->hCursor = NULL;
+	wndClass->hbrBackground = 0;
+	wndClass->lpszMenuName = 0;
+	wndClass->lpszClassName = name;
+}
 
 
 static result_t cdrawRendererPrint_gl(cdrawRenderer_win_gl const* const r)
@@ -120,6 +233,7 @@ static result_t cdrawRendererEndDraw_gl(cdrawRenderer_win_gl* const r, uint32_t 
 static result_t cdrawRendererDisplay_gl(cdrawRenderer_win_gl* const r)
 {
 	// swap buffers
+	BOOL const result = SwapBuffers(r->dc);
 
 	// done
 	return 0;
@@ -150,6 +264,26 @@ static result_t cdrawRendererResize_gl(cdrawRenderer_win_gl* const r, uint32_t c
 static result_t cdrawRendererAttachWindow_gl(cdrawRenderer_win_gl* const r, uint32_t const windowIndex, ptrk_t const p_data)
 {
 	// assign real context to window
+	cdraw_assert(r);
+
+#if CDRAW_TARGET_WINDOWS
+	{
+		// required leading members of "cdrawWindow_win" (cdrawWindow_win.c)
+		struct {
+			HINSTANCE hInst;
+			HWND hWnd;
+			HDC hDC;
+		} const* const data = p_data;
+
+		// activate context
+		r->dc = data->hDC;
+		wglMakeCurrent(r->dc, r->rc);
+		wglSwapIntervalEXT(1);
+
+		// attempt to set pixel format
+		cdrawRendererInternalWindowSetPixelFormat_win_gl(r);
+	}
+#endif // #if CDRAW_TARGET_WINDOWS
 
 	// done
 	return 0;
@@ -158,6 +292,9 @@ static result_t cdrawRendererAttachWindow_gl(cdrawRenderer_win_gl* const r, uint
 static result_t cdrawRendererDetachWindow_gl(cdrawRenderer_win_gl* const r, uint32_t const windowIndex)
 {
 	// remove context from window
+	wglSwapIntervalEXT(0);
+	wglMakeCurrent(r->dc, NULL);
+	r->dc = NULL;
 
 	// done
 	return 0;
@@ -187,18 +324,66 @@ result_t cdrawRendererCreate_gl(cdrawRenderer* const renderer, uint32_t const su
 	result_init();
 	size_t const dataSz = sizeof(cdrawRenderer_win_gl);
 	cdrawRenderer_win_gl* r;
+	cdrawRenderer_win_gl tmpContextFalse = { 0 }, tmpContextTrue = { 0 };
+	HWND hWndFalse = NULL, hWndTrue = NULL;
+	cstrk_t const wndClassName = "cdrawRendererDummyWindow_gl";
+	WNDCLASSA wndClass = { 0 };
+
 	asserterr(renderer && !renderer->r && !renderer->renderAPI, errcode_invalidarg);
 	r = (cdrawRenderer_win_gl*)malloc(dataSz);
 	asserterr_ptr(r, errcode_renderer_init);
 	memset(r, 0, dataSz);
 
 	// register dummy window class
+	cdrawRendererInternalWindowClassInit_win_gl(&wndClass, wndClassName);
+	if (RegisterClassA(&wndClass))
+	{
+		// create window to generate "false" context with pixel format describing capabilities
+		hWndFalse = CreateWindowA(wndClassName, NULL, 0, 0, 0, 0, 0, NULL, NULL, wndClass.hInstance, &tmpContextFalse);
+		if (hWndFalse)
+		{
+			// create window to generate "true" context with actual pixel format
+			tmpContextFalse.rc = wglCreateContext(tmpContextFalse.dc);
+			if (tmpContextFalse.rc)
+			{
+				if (wglMakeCurrent(tmpContextFalse.dc, tmpContextFalse.rc))
+				{
+					// initialize extensions (GPU should do this automatically, but just in case)
+					if (glewInit() == GLEW_OK)
+					{
+						// do it again with selected format
+						tmpContextTrue.pf = tmpContextFalse.pf;
+						hWndTrue = CreateWindowA(wndClassName, NULL, 0, 0, 0, 0, 0, NULL, NULL, wndClass.hInstance, &tmpContextTrue);
+						if (hWndTrue)
+						{
+							tmpContextTrue.rc = wglCreateContext(tmpContextTrue.dc);
+							if (tmpContextTrue.rc)
+							{
+								if (wglMakeCurrent(tmpContextTrue.dc, tmpContextTrue.rc))
+								{
+									// hand off to output so it is not deleted with window
+									r->rc = tmpContextTrue.rc;
+									tmpContextTrue.rc = NULL;
+								}
+							}
 
-	// create window to generate "false" context with pixel format describing capabilities
+							// clean up true context window
+							DestroyWindow(hWndTrue);
+						}
+					}
+				}
 
-	// create window to generate "true" context with actual pixel format
+				// clean up false context
+				wglDeleteContext(tmpContextFalse.rc);
+			}
 
-	// clean up
+			// clean up false context window
+			DestroyWindow(hWndFalse);
+		}
+
+		// clean up dummy class
+		UnregisterClassA(wndClassName, wndClass.hInstance);
+	}
 
 	// all done
 	renderer->r = r;
@@ -214,7 +399,8 @@ result_t cdrawRendererDestroy_gl(cdrawRenderer* const renderer)
 	r = ((cdrawRenderer_win_gl*)renderer->r);
 
 	// release context
-
+	wglMakeCurrent(NULL, NULL);
+	wglDeleteContext(r->rc);
 
 	// all done
 	free(renderer->r);
